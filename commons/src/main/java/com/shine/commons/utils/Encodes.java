5ae4228aa5ba2cac924173a3d81eb396bd6c2c0b
fuyongde <fuyongde@foxmail.com>
2018-05-27 00:47:59 +0800

    update

--- commons/src/main/java/com/shine/commons/utils/Encodes.java
+++ commons/src/main/java/com/shine/commons/utils/Encodes.java
@@ -7,7 +7,7 @@ import java.net.URLEncoder;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 /**
  * 封装各种格式的编码解码工具类.
